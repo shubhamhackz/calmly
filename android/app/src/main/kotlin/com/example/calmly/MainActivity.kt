8ca5554ff8302bf6c7013b8b10b8b142a97bfe59
Shubhamhackz <shubhamhackzz@gamil.com>
2021-02-15 09:31:19 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/calmly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calmly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:calmly/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
