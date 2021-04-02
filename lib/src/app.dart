9ebf098e260f4ae2600f95076ea1dafe894df6a3
Shubhamhackz <shubhamhackzz@gamil.com>
2021-04-02 09:14:45 +0530

    mediaquery issue fixed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,7 +2,7 @@ import 'package:calmly/src/config/theme_config.dart';
 import 'package:calmly/src/screens/home_screen.dart';
 
 import 'package:calmly/src/utils/system_theme.dart';
-import 'package:calmly/src/config/device_config.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,9 +11,6 @@ import 'config/app_state.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //initialize width and height
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
     return Consumer<AppState>(
       builder: (_, appState, __) {
         //change statusbar cxolor
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:calmly/src/components/calm_box/modern_calm_box.dart';
 import 'package:calmly/src/components/calm_box/traditional_calm_box.dart';
 import 'package:calmly/src/components/home_widget.dart';
 import 'package:calmly/src/config/app_state.dart';
+import 'package:calmly/src/config/device_config.dart';
 
 import 'package:provider/provider.dart';
 
@@ -15,6 +16,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    //initialize width and height
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
     return Consumer<AppState>(
       builder: (_, appState, __) {
         return Scaffold(
